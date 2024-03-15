f39781ccc66cf2795142ac8a621c5b38bff06caa
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-03-15 20:32:01 +0000

    Special

---
+++ Special/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            if(n == 0) return 0;
+            else if(n == 1) return 1;
+            else return GetSpecial(n-1) + 2*GetSpecial(n-2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+            Console.WriteLine(GetSpecial(num));
+        }
+
+    }
+}
